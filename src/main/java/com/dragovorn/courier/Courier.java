9006bed7340d997b97b2f456c0f9fafd5d8008fd
Dragovorn <me@dragovorn.com>
2017-11-22 15:10:27 -0500

    base startup code

--- src/main/java/com/dragovorn/courier/Courier.java
+++ src/main/java/com/dragovorn/courier/Courier.java
@@ -4,13 +4,13 @@ import com.dragovorn.courier.gsi.GameStateIntegration;
 import com.dragovorn.courier.log.CourierLogger;
 import com.dragovorn.courier.log.LoggingOutputStream;
 import com.dragovorn.courier.util.FileUtil;
+import com.google.gson.*;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,29 +22,31 @@ public class Courier {
     private Logger logger;
 
     public static File baseDir;
+    public static File configFile;
     public static File logDir;
 
     public Courier() {
-        baseDir = new File(System.getProperty("user.home"), ".courier");
+        baseDir = new File(System.getProperty("user.home"), ".courier"); // Best place to store log files
+        configFile = new File(baseDir, "config.json");
         logDir = new File(Courier.baseDir, "logs");
         baseDir.mkdirs();
         logDir.mkdirs();
         this.logger = new CourierLogger();
 
-        System.setErr(new PrintStream(new LoggingOutputStream(this.logger, Level.SEVERE), true));
+        System.setErr(new PrintStream(new LoggingOutputStream(this.logger, Level.SEVERE), true)); // Make sure everything is set to our logger
         System.setOut(new PrintStream(new LoggingOutputStream(this.logger, Level.INFO), true));
 
         try {
-            TrayIcon icon = new TrayIcon(ImageIO.read(FileUtil.getResource("icon.png")), "Courier");
+            TrayIcon icon = new TrayIcon(ImageIO.read(FileUtil.getResource("icon.png")), "Courier"); // Make us a tray icon, so people can kill our program
 
             PopupMenu menu = new PopupMenu();
             menu.add("Courier v" + Version.getVersion());
             menu.addSeparator();
 
-            MenuItem exit = new MenuItem("Exit");
+            MenuItem exit = new MenuItem("Exit"); // So our program can be stopped
             exit.addActionListener(this::shutdown);
 
-            MenuItem logs = new MenuItem("View Logs Folder");
+            MenuItem logs = new MenuItem("View Logs Folder"); // So those who aren't good w/ technology can easily send log files
             logs.addActionListener(this::openLogs);
 
             menu.add(logs);
@@ -58,6 +60,120 @@ public class Courier {
         }
 
         this.logger.info("Courier v" + Version.getVersion() + " started!");
+
+        if (!configFile.exists()) { // Do initial setup/loading
+            JsonObject config = new JsonObject();
+
+            this.logger.info("New installation detected, going through initial setup!");
+            JFileChooser selector = new JFileChooser(new File(".")); // Ask user for where their dota directory is
+            selector.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            selector.setDialogTitle("Select Dota 2 Folder");
+
+            if (selector.showOpenDialog(Main.invisible) == JFileChooser.APPROVE_OPTION) {
+                File file = selector.getSelectedFile();
+                config.addProperty("directory", file.getAbsolutePath());
+                this.logger.info("User selected: " + file.getAbsolutePath());
+                this.logger.info("Checking to see if the user is being truthful...");
+
+                GSIState state = generateGSI(file);
+
+                if (state == GSIState.CRAETED || state == GSIState.WORKING) {
+                    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                    try {
+                        FileWriter writer = new FileWriter(configFile);
+
+                        gson.toJson(config, writer); // Save the config file
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    this.logger.info("The user is a liar... Shutting down!");
+                    shutdown(null);
+                }
+            } else {
+                this.logger.info("Action cancelled by user! Shutting down!");
+                shutdown(null);
+            }
+        } else {
+            this.logger.info("Found config file! Making sure everything is where it should be!");
+
+            JsonObject config = new JsonObject();
+
+            try {
+                JsonParser parser = new JsonParser();
+                JsonElement jsonElement = parser.parse(new FileReader(configFile));
+                config = jsonElement.getAsJsonObject();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+
+            if (generateGSI(new File(config.get("directory").getAsString())) == GSIState.BROKEN) {
+                this.logger.info("Config file pointing to incorrect directory! Shutting down!");
+                shutdown(null);
+            }
+        }
+
+        this.integration = new GameStateIntegration(322);
+    }
+
+    private GSIState generateGSI(File file) {
+        GSIState state = GSIState.BROKEN;
+
+        file = new File(file, "game"); // Make sure it's actually a dota directory
+
+        if (file.exists()) {
+            file = new File(file, "dota");
+
+            if (file.exists()) {
+                file = new File(file, "cfg");
+
+                if (file.mkdirs()) {
+                    this.logger.info("There appears to be no CFG directory, creating it...");
+                }
+
+                file = new File(file, "gamestate_integration"); // Begin making our important files
+
+                if (file.mkdirs()) {
+                    this.logger.info("Creating gamestate_integration directory...");
+                }
+
+                file = new File(file, "gamestate_integration_courier.cfg");
+
+                if (file.exists()) {
+                    state = GSIState.WORKING;
+                } else {
+                    this.logger.info("Generating GSI file...");
+                    try {
+                        PrintWriter writer = new PrintWriter(file);
+                        writer.println("\"Dota 2 Integration Configuration\"");
+                        writer.println("{");
+                        writer.println("    \"uri\"           \"http://localhost:322/\"");
+                        writer.println("    \"timeout\"       \"5.0\"");
+                        writer.println("    \"buffer\"        \"0.1\"");
+                        writer.println("    \"throttle\"      \"0.1\"");
+                        writer.println("    \"heartbeat\"    \"30.0\"");
+                        writer.println("    \"data\"");
+                        writer.println("    {");
+                        writer.println("        \"provider\"      \"1\"");
+                        writer.println("        \"map\"           \"1\"");
+                        writer.println("        \"hero\"          \"1\"");
+                        writer.println("        \"abilities\"     \"1\"");
+                        writer.println("        \"items\"         \"1\"");
+                        writer.println("    }");
+                        writer.println("}");
+                        writer.close(); // Literally most important part of this 'generation' code
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    JOptionPane.showMessageDialog(null, "If you have Dota 2 currently running please restart it.", "Information", JOptionPane.INFORMATION_MESSAGE);
+                    state = GSIState.CRAETED;
+                }
+            }
+        }
+
+        return state;
     }
 
     /*
@@ -78,9 +194,16 @@ public class Courier {
      */
     private void openLogs(ActionEvent event) {
         try {
+            this.logger.info("User wants to see logs folder, tidy up lads, we're having people over!");
             Desktop.getDesktop().open(logDir);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private enum GSIState {
+        WORKING,
+        CRAETED,
+        BROKEN
     }
 }
--- src/main/java/com/dragovorn/courier/Main.java
+++ src/main/java/com/dragovorn/courier/Main.java
@@ -1,11 +1,24 @@
 package com.dragovorn.courier;
 
+import com.dragovorn.courier.util.FileUtil;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class Main {
 
+    public static JFrame invisible;
+
     public static void main(String... args) {
+        invisible = new JFrame();
+        try {
+            invisible.setIconImage(ImageIO.read(FileUtil.getResource("icon.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         if (!SystemTray.isSupported() || !Desktop.isDesktopSupported()) { // We do this because if there is no sys-tray icon users can't terminate the program, which would be bad
             JOptionPane.showMessageDialog(null, "Your platform isn't supported currently!", "Unsupported platform!", JOptionPane.ERROR_MESSAGE);
             return;
